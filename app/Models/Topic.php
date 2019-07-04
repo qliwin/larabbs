5780129a8687053488e754b5c08ec06be25badc0
qliwin <qliwin@163.com>
2019-07-04 14:22:22 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,7 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Category;
+use App\Models\User;
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // 一个话题属于一个分类
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // 一个话题属于一个用户
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
