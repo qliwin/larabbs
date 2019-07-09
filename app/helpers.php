ba6fe8c845b4a89f5af93273a8f4913d9cde5790
qliwin <qliwin@163.com>
2019-07-09 09:36:55 +0800

    模型观察器添加摘要

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,11 @@ function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+// 生成摘要
+function make_excerpt($value, $length = 200)
+{
+    // strip_tags:从字符串中去除 HTML 和 PHP 标记
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
